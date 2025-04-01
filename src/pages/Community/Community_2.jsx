6b81603dd9328e38afadd9e742e4be55b1cbf311
Ahmed Yasser Singap <130315264+singap88a@users.noreply.github.com>
2025-04-02 00:47:26 +0200

    900

--- src/pages/Community/Community_2.jsx
+++ src/pages/Community/Community_2.jsx
@@ -1,11 +1,11 @@
- import Community_Header from "../../components/Community2/community_Header"
+// import Community_Header from "../../components/Community2/community_Header"
 import Community_Body from "../../components/Community2/Community_Body"
-
+ 
  
 function Community_2() {
   return (
     <div>
-       <Community_Header />
+       {/* <Community_Header /> */}
       <Community_Body />
     </div>
   )
